8c726211ceda18b8d311377f160ba6f21bf077b0
wahyukrishna <78397628+wahyukrishna@users.noreply.github.com>
2021-02-03 04:36:05 +0700

    Migrasi HTML,CSS ke React

--- src/App.js
+++ src/App.js
@@ -2,9 +2,40 @@ import "./styles.css";
 
 export default function App() {
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+    <div className="container">
+      <div className="frame">
+        <section className="header">
+          <button className="header-btn main-black-color">Add</button>
+          <h1 className="header-title">Todo List</h1>
+          <button className="header-btn main-red-color">Clear</button>
+        </section>
+        <section className="add">
+          <form className="add-form">
+            <input type="text" className="add-input"></input>
+            <button className="add-btn main-black-color">Add</button>
+          </form>
+        </section>
+        <section className="todos">
+          <div className="todo">
+            <span className="todo-text">Learning React</span>
+          </div>
+          <div className="todo">
+            <span className="todo-text">Learning React</span>
+          </div>
+          <div className="todo">
+            <span className="todo-text">Learning React</span>
+          </div>
+          <div className="todo">
+            <span className="todo-text">Learning React</span>
+          </div>
+          <div className="todo">
+            <span className="todo-text">Learning React</span>
+          </div>
+          <div className="todo">
+            <span className="todo-text">Learning React</span>
+          </div>
+        </section>
+      </div>
     </div>
   );
 }
