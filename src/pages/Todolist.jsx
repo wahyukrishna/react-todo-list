73aebedae35199378f1f5eeed3818504c7394a91
wahyukrishna <78397628+wahyukrishna@users.noreply.github.com>
2021-02-03 16:48:28 +0700

    delete function dan placeholder finishing crud

--- src/pages/Todolist.jsx
+++ src/pages/Todolist.jsx
@@ -31,13 +31,19 @@ const Todolist = () => {
     setTodos(addedTodo);
   };
 
+  const clearTodos = () => setTodos([]);
+
   const showAddToggle = () => setShowAdd(!showAdd);
 
   console.log("todos", todos);
 
   return (
     <Paper>
-      <Header showAddToggle={showAddToggle} showAdd={showAdd} />
+      <Header
+        showAddToggle={showAddToggle}
+        showAdd={showAdd}
+        clearTodos={clearTodos}
+      />
       <TodoForm addTodo={addTodo} showAdd={showAdd} />
       <Todos todos={todos} completeTodo={completeTodo} />
     </Paper>
