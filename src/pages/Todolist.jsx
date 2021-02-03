fae93578877669f68aa83bb561671301855bfcb5
wahyukrishna <78397628+wahyukrishna@users.noreply.github.com>
2021-02-03 15:51:34 +0700

    complete todo function membuat strike through todo apabila is complete nya true atau sebaliknya

--- src/pages/Todolist.jsx
+++ src/pages/Todolist.jsx
@@ -7,28 +7,35 @@ import Todos from "../components/Todos";
 
 const Todolist = () => {
   const [todos, setTodos] = useState([
-    { text: "Belajar React!" },
-    { text: "Belajar props di React" },
-    { text: "Belajar React Hooks" }
+    { text: "Belajar React!", isCompleted: false },
+    { text: "Belajar props di React", isCompleted: false },
+    { text: "Belajar React Hooks", isCompleted: false }
   ]);
 
   const [showAdd, setShowAdd] = useState(false);
 
   const addTodo = (value) => {
-    const addedTodo = [...todos, { text: value }];
+    const addedTodo = [...todos, { text: value, isCompleted: false }];
+
+    setTodos(addedTodo);
+  };
+
+  const completeTodo = (index) => {
+    const addedTodo = [...todos];
+    addedTodo[index].isCompleted = !addedTodo[index].isCompleted;
 
     setTodos(addedTodo);
   };
 
   const showAddToggle = () => setShowAdd(!showAdd);
 
-  console.log("showAdd", showAdd);
+  console.log("todos", todos);
 
   return (
     <Paper>
       <Header showAddToggle={showAddToggle} showAdd={showAdd} />
       <TodoForm addTodo={addTodo} showAdd={showAdd} />
-      <Todos todos={todos} />
+      <Todos todos={todos} completeTodo={completeTodo} />
     </Paper>
   );
 };
