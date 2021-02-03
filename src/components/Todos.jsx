b2237a860fe1d567d8d199f05a36c5792fecdb02
wahyukrishna <78397628+wahyukrishna@users.noreply.github.com>
2021-02-03 11:48:08 +0700

    refactor todos,todolist,component

--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -1,20 +1,8 @@
 import React from "react";
 import Todo from "./Todo";
+import PropTypes from "prop-types";
 
-const Todos = () => {
-  const todos = [
-    { text: "Belajar React!" },
-    { text: "Belajar props di React" },
-    { text: "Belajar React Hooks" },
-    { text: "Belajar state di React " },
-    { text: "Belajar React" },
-    { text: "Belajar React!" },
-    { text: "Belajar props di React" },
-    { text: "Belajar React Hooks" },
-    { text: "Belajar state di React " },
-    { text: "Belajar React" }
-  ];
-
+const Todos = ({ todos }) => {
   return (
     <section className="todos">
       {todos.map((todo) => {
@@ -22,6 +10,14 @@ const Todos = () => {
       })}
     </section>
   );
+};
+
+Todos.propTypes = {
+  todos: PropTypes.arrayOf(
+    PropTypes.shape({
+      text: PropTypes.string
+    })
+  )
 };
 
 export default Todos;
--- src/pages/Todolist.jsx
+++ src/pages/Todolist.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Paper from "../components/Paper";
 import Header from "../components/Header";
@@ -6,11 +6,16 @@ import TodoForm from "../components/TodoForm";
 import Todos from "../components/Todos";
 
 const Todolist = () => {
+  const [todos, setTodos] = useState([
+    { text: "Belajar React!" },
+    { text: "Belajar props di React" },
+    { text: "Belajar React Hooks" }
+  ]);
   return (
     <Paper>
       <Header />
       <TodoForm />
-      <Todos />
+      <Todos todos={todos} />
     </Paper>
   );
 };
