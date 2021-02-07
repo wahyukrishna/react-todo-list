d370c2a872c08e0a11dbf92a701bbd0d3385795c
wahyukrishna <78397628+wahyukrishna@users.noreply.github.com>
2021-02-07 21:11:16 +0700

    finish refactor emotion js all components

--- src/components/todos/Todos.jsx
+++ src/components/todos/Todos.jsx
@@ -1,12 +1,18 @@
-import React from "react";
+/** @jsxImportSource @emotion/react */
+
+// import { jsx } from "@emotion/react";
+// import { useTheme } from "@emotion/react";
+// import React from "react";
 import Todo from "../todo/Todo";
 import PropTypes from "prop-types";
 
-import styles from "./todos.module.css";
+// import styles from "./todos.module.css";
+import * as styles from "./todos.styles";
 
 const Todos = ({ todos, completeTodo }) => {
+  // const theme = useTheme();
   return (
-    <section className={styles.todos}>
+    <section css={styles.todos}>
       {todos.length > 0 &&
         todos.map((todo, index) => {
           return (
@@ -20,10 +26,10 @@ const Todos = ({ todos, completeTodo }) => {
           );
         })}
       {todos.length === 0 && (
-        <div className={styles.todoPlaceholderText}>
+        <div css={styles.todoPlaceholderText}>
           Add Todo by clicking{" "}
-          <span className={styles.addButtonPlaceholderText}>Add</span> button on
-          the top left corner
+          <span css={styles.addButtonPlaceholderText}>Add</span> button on the
+          top left corner
         </div>
       )}
     </section>
